73ac4cabf332fef56a97e2676862620568d71431
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2024-12-25 08:49:46 +0700

    u

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -23,6 +23,11 @@ const Sidebar = () => {
 
   // Menu items cho admin
   const adminMenuItems = [
+    { 
+      name: 'Trang chủ', 
+      path: '/',
+      icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' 
+    },
     {
         name: 'Thống kê',
         path: '/admin',
@@ -42,6 +47,11 @@ const Sidebar = () => {
 
   // Menu items cho giáo viên
   const teacherMenuItems = [
+    { 
+      name: 'Trang chủ', 
+      path: '/',
+      icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' 
+    },
     { 
       name: 'Thống kê', 
       path: '/teacher/',
